c0a52c2a06cc695b4eea0bf54f9f6d31f85dce61
Mahima Gangavarapu <39587007+gmahima@users.noreply.github.com>
2019-08-05 21:28:33 +0530

    return null from setState

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Teacher from './Teacher';
+import Teacher from "./Teacher";
 
 class App extends Component {
-
   state = {
-    teacher: ''
+    teacher: ""
   };
 
   updateTeacher = teacher => {
-    this.setState({ teacher });
-  }
+    const newTeacher = teacher;
+    this.setState(prevState => {
+      if (prevState.teacher === newTeacher) {
+        return null;
+      } else {
+        return { teacher };
+      }
+    });
+  };
 
   render() {
-    const teachers = ['jay', 'vivianne', 'ecma', 'json'];
+    const teachers = ["jay", "vivianne", "ecma", "json"];
 
     return (
       <div className="App">
         <header>
           <h1>Meet the Teachers</h1>
-          {teachers.map((teacher,i) =>
+          {teachers.map((teacher, i) => (
             <button
               key={teacher}
               type="button"
               value={teacher}
               onClick={e => this.updateTeacher(e.target.value)}
-            >{teacher}
-            </button>)}
+            >
+              {teacher}
+            </button>
+          ))}
         </header>
 
         <Teacher teacher={this.state.teacher} />
